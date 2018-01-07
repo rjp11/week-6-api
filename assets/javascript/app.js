7c1e333a68b8e7713f9bdbdc2e6e3fa40ac8a690
Ryan Parks <rjp@ryans-mbp.attlocal.net>
2018-01-07 14:19:04 -0600

    submitted version with comments and adjusted styling for small and xs screens

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,6 +18,7 @@ var topics = [
 ];
 var resultCount = 0;
 
+// when topic button is clicked, access gifs from the giphy API
 $(document).on("click", "button", function () {
     resultCount = 0;
     var search = $(this).text();
@@ -29,21 +30,31 @@ $(document).on("click", "button", function () {
         })
         .done(function (response) {
             var results = response.data;
+            // empty the gif div to replace the images with the new query
             $(".gifs").empty();
             for (var j = 0; j < results.length; j++) {
+                // limit displayed gifs to 10. 
                 if (resultCount < 10) {
+                    // only display gifs if more family friendly than pg 13
                     if (results[j].rating !== "r" && results[j].rating !== "pg-13") {
                         var gifDiv = $("<div class='item'>");
                         var rating = results[j].rating;
                         var p = $("<h4>").text("Rating: " + rating);
                         var gifImage = $("<img>");
+                        // apply attribute for source of still image url
                         gifImage.attr("data-still", results[j].images.fixed_height_still.url)
+                        // apply attribute for source of animated gif
+                        // fixed height chosen so results display cleanly together
                         gifImage.attr("data-gif", results[j].images.fixed_height.url);
+                        // attribute in order to switch between still and animated
                         gifImage.attr("data-status", 0);
+                        // set initial source to still image
                         gifImage.attr("src", results[j].images.fixed_height_still.url);
+                        //add gif and text to div and append to DOM
                         gifDiv.append(gifImage);
                         gifDiv.append(p);
                         $(".gifs").prepend(gifDiv);
+                        // increase result count to limit to 10 results
                         resultCount++;
                     };
                 };
@@ -51,16 +62,21 @@ $(document).on("click", "button", function () {
         });
 });
 
+// event handler to switch gif source between still and animated on click
 $(document).on("click", "img", function () {
+    //if still change image to animated
     if ($(this).attr("data-status") === "0") {
         $(this).attr("src", $(this).attr("data-gif"));
         $(this).attr("data-status", 1);
-    } else {
+    } 
+    // else change image from animated to still
+    else {
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-status", 0);
     };
 });
 
+// function to render the buttons to the DOM from the topics array
 function renderButtons() {
     $(".buttons").empty();
     for (var i = 0; i < topics.length; i++) {
@@ -68,20 +84,22 @@ function renderButtons() {
     };
 };
 
-
-
+// event handler to add a new topic/button to the possible Giphy API search options
 $("#add-topic").on("click", function (event) {
     var newTopic = $("#topic-input").val().trim();
+    // prevent duplicate buttons by checking the index of the form
     if (topics.indexOf(newTopic) === -1) {
         event.preventDefault();
         topics.push(newTopic);
-        console.log(topics);
         renderButtons();
+        // reset form input to zero after adding button
         $("#topic-input").val('');
     } else {
         event.preventDefault();
+        // reset form input to blank after entering a duplicate
         $("#topic-input").val('');
     };
 });
 
+// on page load, render default topic buttons to the DOM.
 renderButtons();
